f3d2ca4b236bc250aeb202d6cb09d410dfb13eee
Harold Rojas <harolrop14@gmail.com>
2022-04-01 19:38:56 -0400

    basics pipes end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SalesModule } from './sales/sales.module';
 import { SharedModule } from './shared/shared.module';
+
+//Cambiar locale de la app
+import localeEs from '@angular/common/locales/es-PE';
+import localeJa from '@angular/common/locales/ja';
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeEs);
+registerLocaleData(localeJa);
 
 @NgModule({
   declarations: [
@@ -16,7 +23,9 @@ import { SharedModule } from './shared/shared.module';
     SalesModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-PE'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
