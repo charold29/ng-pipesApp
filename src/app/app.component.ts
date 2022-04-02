17c4264ea78d447e400f1d93dee7a5b8bfe1a936
Harold Rojas <harolrop14@gmail.com>
2022-04-01 21:35:13 -0400

    template de appcomponent.ts y espacios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
-  name :string = 'Harold ROJAS';
-  number :number = 29091999;
-  obj =  {
-    name: 'Harold',
-    lastname: 'Rojas'
-  }  
-
-  showName(){
-    console.log(this.name);
-    console.log(this.number);
-    console.log(this.obj);
-  }
-
+  
 }
